4a5ded3229e29b499b56701b7d26ccdb12f61a14
Douglas Luman <dluman@douglasjluman.com>
2020-02-05 21:58:45 -0500

    Initial commit

---
+++ src/main/java/labfour/HatPurchase.java
@@ -0,0 +1,57 @@
+package labfour;
+
+/** Divides G. Wiz's lottery winnings with their friends.
+ *
+ * @author {Your Name Here}
+ */
+
+public class HatPurchase {
+  
+  /** Entry point.
+   *
+   * @param args The command line arguments
+   */
+  public static void main(String[] args) {
+    // Initial amounts
+    /*
+     * TODO: Set up initial amounts and print statement
+     */
+    
+    // Take out taxes
+     /*
+     * TODO: Set tax rate and take taxes out of winnings
+     */
+    
+    // Buy some CrocScout cookies
+     /*
+     * TODO: Subtract the cost of cookies and taxes
+     */
+     
+    // Buys the latest edition of The Clawparazzi
+     /*
+     * TODO: Reduce G. Wiz's winnings by the cost of the magazine,
+     *       plus tax
+     */
+    
+    // Pay Wizard Union dues
+     /*
+     * TODO: Take out union dues and a late fee
+     */
+    
+    // Take out shares owed to friends
+     /*
+     * TODO: Split what's left with G. Wiz's friends
+     */
+    
+    // Does G. Wiz have enough to buy his hat?
+     /*
+     * TODO: Find out if G. Wiz can buy the hat
+     */
+    
+    // Conclusion to our thrilling tale of financial frenzy
+     /*
+     * TODO: Print the final line reporting if G. Wiz can afford
+     *       the hat
+     */
+  }
+}
---
+++ writing/reflection.md
@@ -0,0 +1,17 @@
+# Lab 4 reflection: {Your Name Here}
+
+TODO: Respond to reflection questions
+
+## Why was defining the `taxRate` identifier helpful? What does this say about using variables to store data?
+
+## What is the difference between the `double` and `int` data types? 
+
+### If you used an `int` data type anywhere in your program, why did you choose it? If not, why didn't you find `int` helpful?
+
+## Reflect on your use of comments in this program. How did using comments help?
+
+## Paste your `boolean` assignment below. How does it work?
+
+```java
+
+```
